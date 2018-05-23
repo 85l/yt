1bf91e1481699001bdb32905d5f04c1b16e0bea8
salehjaradat12 <39270748+salehjaradat12@users.noreply.github.com>
2018-05-23 17:03:02 +0300

    Create style-v2.js

---
+++ style-v2.js
@@ -0,0 +1,117 @@
+*, *::before, *::after {
+  -webkit-box-sizing: border-box;
+          box-sizing: border-box;
+}
+body{font-family:neo!important;background-color:#f5f5f5!important}
+a{font-family:neo!important;}
+
+body {
+  font-family: neo;
+  text-align: center;
+  width: 100%;
+  margin: 0 auto;
+  padding: 60px 10%;
+}
+
+
+input[type="text"]{font-family:neo!important;}
+
+
+.form {
+  display: block;
+  margin-bottom: 10px;font-family:neo!important;
+  position: relative;
+}
+
+p {
+  font-size: 13px;
+  line-height: 1.5;font-family:neo!important;
+  margin: 0;
+  color: #bbb;
+}
+p strong {
+  font-weight: bold;font-family:neo!important;
+  color: #777;
+}
+
+button {
+  
+  
+  font-family:neo!important;
+  position: absolute;
+  border: 0;
+  top: 0;
+  left: -2px;
+  height: 60px;
+  width: 90px;
+  padding: 0;
+  -webkit-appearance: none;
+     -moz-appearance: none;
+          appearance: none;
+  padding: 0;
+  margin: 0;
+  line-height: 60px;
+  font-weight: bold;
+  background: #2196F3;
+  color: #fff;
+  font-size: 20px;
+  border-radius: 10px 0 0 10px;
+  cursor: pointer;
+}
+button:hover, button:focus {
+  background: #0c7cd5;
+}
+
+.video iframe {
+  display: block;
+}
+
+input {
+  outline: 0;
+  height: 60px;
+  line-height: 60px;
+    padding-left: 82%;
+  font-size: 20px;
+  width: 100%;
+  display: block;
+  border: 2px solid #2196F3;font-family:neo!important;
+  border-radius: 10px;
+}
+input:focus {
+  background-color: #ffffcc;font-family:neo!important;
+}
+
+.images {
+  padding-top: 20px;
+  margin-top: 20px;
+  border-top: 2px solid #eee;
+}
+.images .img {
+  display: block;
+  max-width: 100%;
+  vertical-align: top;
+  margin: 0 auto;
+  background: #fff;
+  border-top: 1px solid #f5f5f5;
+  -webkit-box-shadow: 0 1px 3px rgba(0, 0, 0, 0.2);
+          box-shadow: 0 1px 3px rgba(0, 0, 0, 0.2);
+  padding: 1%;
+  border-radius: 5px;
+}
+.images .img[src=""] {
+  display: none;
+}
+.images .img + span {
+  display: inline-block;
+  background: #fff;
+  color: #2196F3;
+  font-size: 11px;
+  line-height: 1;
+  padding: 5px 8px;
+  border-radius: 5px;
+  -webkit-box-shadow: 0 1px 10px rgba(0, 0, 0, 0.1);
+          box-shadow: 0 1px 10px rgba(0, 0, 0, 0.1);
+  position: relative;
+  top: -42px;
+  margin-bottom: 40px;
+}
